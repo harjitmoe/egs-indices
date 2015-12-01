4e8b72354e1138705203cb5a0eceba865c3b4c44
HarJIT <harjit@harjit.moe>
2015-12-01 20:22:34 +0000

    Opensource the test* files.
    Signed-off-by: HarJIT <harjit@harjit.moe>

--- test_megadb_compare.py
+++ test_megadb_compare.py
@@ -1,3 +1,9 @@
+# Written in 2015 by HarJIT
+#
+# This file is made available under the CC0 Public Domain Dedication.  To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this file to the public domain worldwide. This file is distributed without any warranty.
+#
+# You may have received a copy of the CC0 Public Domain Dedication along with this file. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
+
 import pprint
 pprint.pprint(eval(open("SbMegaDb.txt","rU").read()),open("SbMegaDb pprint.txt","w"))
 pprint.pprint(eval(open("SbMegaDb old.txt","rU").read()),open("SbMegaDb old pprint.txt","w"))
