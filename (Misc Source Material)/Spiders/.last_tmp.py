1b2ac363f0dc6898af999c4d7cd26602c19d6f08
HarJIT <harjit@harjit.moe>
2015-12-19 21:33:52 +0000

    Update data, regrab certain.
    Signed-off-by: HarJIT <harjit@harjit.moe>

--- (Misc Source Material)/Spiders/.last_tmp.py
+++ (Misc Source Material)/Spiders/.last_tmp.py
@@ -52,59 +52,70 @@ import re, os, time
 
 i = 1
 dab={}
-if os.path.exists("b.txt"):
-    f=open("b.txt","rU")
-    dab=eval(f.read())
-    f.close()
 #wget="..\\wget.exe"
 wget="wget-gnu"
 names={"index":"story","egsnp":"np","sketchbook":"sketch"}
 
-f=open("metadataegs3.txt")
-dob=eval(f.read())
-f.close()
+if os.path.exists("a.txt"):
+    f=open("a.txt")
+    dab=eval(f.read())
+    f.close()
 
 for interface in ("index","egsnp","sketchbook"):
-    for styla in ("DateInBrowserTitle","DateStatedAboveComic"):
-        fs=0
-        sub=dob[names[interface]]
-        print "I",interface
-        while 1:
-            if i not in sub.keys():
-                        print "F",i
-                        fs+=1
-                        if fs>=5:
-                            i=1
-                            break
-                        else:
-                            i+=1
-                            continue
-            else:
-                fs=0
-            try:
-                if (names[interface]+"-"+styla+"-"+str(i) not in dab.keys()) or (dab[names[interface]+"-"+styla+"-"+str(i)]==None):
-                    url = "http://www.egscomics.com/"+interface+".php?date="+(sub[i][styla])
-                    #time.sleep(0.5)
-                    os.system(wget+" -O \""+interface+".php@id="+str(i)+"\" \""+url+"\" >> wgetlog.txt 2>&1")
-                    if not os.path.exists(interface+".php@id="+str(i)):
-                        print "A"
+    fs=0
+    print "I",interface
+    while 1:
+        try:
+            if names[interface]+"-"+str(i) not in dab.keys():
+                url = "http://www.egscomics.com/"+interface+".php?id="+str(i)
+                #time.sleep(0.5)
+                os.system(wget+" -O \""+interface+".php@id="+str(i)+"\" \""+url+"\" >> wgetlog.txt 2>&1")
+                if not os.path.exists(interface+".php@id="+str(i)):
+                    print "A"
+                    continue
+                rd = open(interface+".php@id="+str(i),"rU")
+                data = rd.read()
+                rd.close()
+                os.unlink(interface+".php@id="+str(i))
+                res = re.search("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]</title>", data)
+                res2 = re.search("comics/[-a-zA-Z0-9_()]*.(jpg|gif|png)", data)
+                if not res2:
+                    print "F",i
+                    fs+=1
+                    if fs>=5:
+                        i=1
+                        break
+                    else:
+                        i+=1
                         continue
-                    rd = open(interface+".php@id="+str(i),"rU")
-                    data = rd.read()
-                    rd.close()
-                    os.unlink(interface+".php@id="+str(i))
-                    res2 = re.search("comics/[-a-zA-Z0-9_()]*.(jpg|gif|png)", data)
-                    works=sub[i][styla]
-                    if not res2:
-                        works=None
-                    dab[names[interface]+"-"+styla+"-"+str(i)]=(works,res2.group() if res2 else None)
                 else:
                     fs=0
-            except Exception,e:
-                print "Pfail",i,str(e)
-            i += 1
+                comment=data.split('<div id="newsarea">',1)[1].split('<div id="boxad">',1)[0]
+                date1=""
+                if res:
+                    date1=res.group()[:-8]
+                title=""
+                date2=""
+                if 'title="' in data:
+                    date2,title=(data.split('title="',1)[1].split('"',1)[0]+"-").split("-",1)
+                    date2=date2.strip()
+                    title=title.strip().rstrip("-")
+                date3=""
+                if '">Comic for ' in data:
+                    date3=data.split('">Comic for ',1)[1].split("</div>",1)[0].split(", ",1)[1]
+                    date3=date3.replace(", "," ")
+                    month,day,year=date3.split(" ",3)
+                    month="%02d"%([None,"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].index(month[:3]))
+                    day="%02d"%int(day)
+                    date3=year+"-"+month+"-"+day
+                dab[names[interface]+"-"+str(i)]=(date1,date2,date3,title,comment)
+            else:
+                fs=0
+        except Exception,e:
+            print "Pfail",i,str(e)
+        i += 1
 
-f=open("dateswork.txt","w")
+f=open("metadataegs2.txt","w")
 f.write(`dab`)
 f.close()
 
