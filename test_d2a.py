c11fed8f4741595ab4123dfa5ebc280e03f55fa1
HarJIT <harjit@harjit.moe>
2015-12-21 22:20:12 +0000

    Use MDE3, not MDE2, for scanning for discrepencies, and use templatenotice.
    Signed-off-by: HarJIT <harjit@harjit.moe>

--- test_d2a.py
+++ test_d2a.py
@@ -26,23 +26,22 @@
 #  3. The text of this notice must be included, unaltered, with any distribution.
 #
 
-f=open("metadataegs2.txt","r")
-id2date=eval(f.read())
+f=open("metadataegs3.txt","r")
+id2date3=eval(f.read())
 f.close()
 date2id=eval(open("Date2Id.txt","rU").read())
 
 for category in ("story","sketch","np"):
     d2i=date2id[category]
     for date in sorted(d2i.keys()):
-        index=category+"-"+str(d2i[date])
-        if index in id2date:
-            roundtrip1=id2date[index][0]
-            roundtrip2=id2date[index][1]+"-"+id2date[index][3].split(" - ")[0]
-            roundtrip3=id2date[index][2]
-            roundtrip=(roundtrip3 if roundtrip3 else roundtrip1)
-            if roundtrip1!=roundtrip2:
-                print "Eh?",`category`,`date`,`d2i[date]`,`roundtrip1`,`roundtrip2`,`roundtrip3`
+        index=d2i[date]
+        if index in id2date3[category]:
+            roundtrip1=id2date3[category][index]["DateInBrowserTitle"]
+            roundtrip2=id2date3[category][index]["DateStatedAboveComic"]
+            roundtrip=(roundtrip2 if roundtrip2 else roundtrip1)
+            if roundtrip1!=roundtrip:
+                print "Eh?",`category`,`date`,`d2i[date]`,`roundtrip1`,`roundtrip2`
             if date not in (roundtrip,):
-                print "Discrepancy",`category`,`date`,`d2i[date]`,`roundtrip1`,`roundtrip2`,`roundtrip3`
+                print "Discrepancy",`category`,`date`,`d2i[date]`,`roundtrip1`,`roundtrip2`
         else:
             print "Not grabbed",category,date,d2i[date]
