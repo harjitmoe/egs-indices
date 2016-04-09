a785d0fe201a5df98f17c2eb1600f0d166cc31e6
HarJIT <harjit@harjit.moe>
2016-04-09 20:58:51 +0100

    Update.  Collect (but not use yet) indexability data for all possible dates 2013-2016 (although this checks up to the end of the year 2016, including dates which could not be possible yet.

---
+++ (Misc Source Material)/Spiders/egsnp.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>El Goonish Shive - EGS:NP - 2016-04-08 - Outsourcing</title>
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:title" content="El Goonish Shive - EGS:NP - 2016-04-08 - Outsourcing" />
+<meta name="twitter:site" content="@Dantheshive" />
+<meta name="twitter:creator" content="@Dantheshive" />
+
+<link rel="stylesheet" type="text/css" href="styles2.css" />
+<script type="text/javascript">
+<!--
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-12274756-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+//-->
+</script>
+<script type="text/javascript">
+/*
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//buzz.thehiveworks.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 10]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+*/
+</script>
+
+</head>
+<body>
+<div id="wrapper">
+<div id="header"><a href="http://www.egscomics.com/" id="logo"><img src="images/logo2.gif" border=0 /></a><div id="leaderboard">
+    <ins data-revive-zoneid="192" data-revive-id="6eaa2a9eeb707d25b408363e77f1c8b1"></ins>
+<script async src="//ads.thehiveworks.com/delivery/asyncjs.php"></script>
+</iframe></div></div>
+<div id="menu">
+	<a href="newreaders.php" id="newreaders"></a><a href="sketchbook.php" id="sketchbook"></a><a href="egsnp.php" id="egsnp"></a><a href="archives.php" id="archives"></a><a href="faq.php" id="faq"></a><a href="store.php" id="store"></a><a href="extras.php" id="extras"></a><a href="contact.php" id="contact"></a>
+</div><script language="javascript">
+function changePage(id){
+	var location = "egsnp.php?id=" + id;
+	window.location.href=location;
+}
+</script>
+<div style="clear:both; height:15px;"></div>
+<div style="width:960px; background:#fff; clear:both;">
+<div id="comicbody"><img title="2016-04-08 - Outsourcing" src="comics/../comics/1460092053-NP160408_RS_09_outsourcing.png" id="comic" border="0" /><br /></div><div class="nav"><a href="/egsnp.php?id=12" class="first" rel="start"></a><a href="/egsnp.php?id=443" class="prev" rel="prev"></a><div style="width:300px; float:left;font-family:Georgia, Times New Roman, Times, serif;font-size:12pt;text-align:center;font-weight:bold; margin:10px;"><select name="comic" onChange="changePage(this.value)" style="float:left; width:300px;"><option value="">Select a comic...</option><optgroup label="EGS:NP"><option value="12">Tedd Forgot To Do Something</option><option value="18">Grace the Goddess</option><option value="33">Magic Comic Shop</option><option value="42">Not a Date at the Mall</option><option value="180">Back</option><option value="142">Oblivious Hand-Waving</option><option value="121">EGS-Con 2006</option><option value="96">Dan in the MUD</option><option value="81">Respect</option><option value="60">Goonmanji</option><option value="54">Non-Gratuitous Nudity</option><option value="27">Blank Check for Weirdness</option><option value="168">Standalone Jan 2010</option><option value="1">Revenge of the Jokes</option><option value="163">Christmas Intermission</option><option value="157">TF Busters #1</option><option value="164">TF Busters #1 (Cont.)</option><option value="181">Playing With Dolls</option><option value="254">Assorted 2015 01</option><option value="259">Zombie Plans</option><option value="268">Question Mark</option><option value="283">Question Mark Pt 2</option><option value="287">Question Mark Pt 3</option><option value="305">Assorted 2015 02</option><option value="310">Gaming Webcomic</option><option value="325">Assorted 2015 03</option><option value="358">MV5</option><option value="436">Rad Squirrel</option></optgroup></select></div><div class="navspace2"><div class="nextdis"></div><div class="lastdis"></div></div></div><div style="clear:both; height:15px;"></div>
+</div>
+<div style="clear:both; height:15px;"></div>
+<div id="leftarea">
+<hr style="height:2px; background-color:#CCCCCC; border:none; clear:both;" />
+<div id="news">
+<h3>Commentary</h3>
+<div id="newsarea"><div id="newsheader"></div><p>This comic&nbsp;<em>barely</em> exaggerates how easily your character gets given important assignments from the military of a government they&#39;re not even a citizen of.</p>
+<p>The in-game government being referred to here is known as the New California Republic (NCR), and they have limited control of select areas of the region your character is in. Most of the area is independent settlements, and it&#39;s made pretty clear that your character is not a citizen the NCR.</p>
+<p>Granted, the game has a faction reputation system, and you have to be BFFs with them to do some things, but&nbsp;&nbsp;all things considered, they are rather trusting of you,.</p></div></div>
+<div id="boxad">
+    <ins data-revive-zoneid="194" data-revive-id="6eaa2a9eeb707d25b408363e77f1c8b1"></ins>
+<script async src="//ads.thehiveworks.com/delivery/asyncjs.php"></script>
+</div>
+</div><div id="rightarea">
+<div id="righttower">
+
+    <ins data-revive-zoneid="193" data-revive-id="6eaa2a9eeb707d25b408363e77f1c8b1"></ins>
+<script async src="//ads.thehiveworks.com/delivery/asyncjs.php"></script>
+
+</div>
+<p>
+				<a href="http://twitter.com/dantheshive"><img src="/img/twitter.gif" alt="twitter" height="64" border="0" width="160"></a>
+			</p>
+		
+					<p>
+				</p><p><a href="http://www.patreon.com/egscomics"><img src="/images/patreon.png"></a></p>
+			<p><a href="http://www.thehiveworks.com/"><img src="images/hw-logo-egs.png" border="0"  /></a></p>
+
+			<p>
+				<a href="/rss.php">
+				<img src="/img/rss.gif" alt="rss" title="rss" height="17" border="0" width="36">
+				</a>
+			</p></div>
+<div style="clear:both;"></div>
+</div>
+<div id="footer">All comics, artwork, characters copyright &copy; 2002-2015 by Dan Shive.<br />
+ El Goonish Shive is a comic about a group of teenagers who face both real life and bizarre, supernatural situations.
+			<br>
+			It is a comedy mixed with drama and is recommended for audiences thirteen and older.
+			<br>
+			<a href="/contact.php">Contact Dan Shive</a> - <a href="javascript:buildreportform();">Report an AD problem</a>
+</div>
+<!--<noscript><p><img src="//buzz.thehiveworks.com/piwik.php?idsite=10" style="border:0;" alt="" /></p></noscript>-->
+<!-- Quantcast Tag -->
+<script type="text/javascript">
+var _qevents = _qevents || [];
+
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+
+_qevents.push({
+qacct:"p-q7HpATVu6wS12"
+});
+</script>
+
+<noscript>
+<div style="display:none;">
+<img src="//pixel.quantserve.com/pixel/p-q7HpATVu6wS12.gif" border="0" height="1" width="1" alt="Quantcast"/>
+</div>
+</noscript>
+<!-- End Quantcast tag -->
+</body>
+</html>
