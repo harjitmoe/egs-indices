84aaedd8c929b6a7e1490e0f552d5d31651d2cca
HarJIT <harjit@harjit.moe>
2017-07-10 15:42:36 +0100

    Did I really commit that in that state?

--- out/purifarticle.py
+++ out/purifarticle.py
@@ -8,59 +8,59 @@ for sect in ("ST","SB","NP"):
 
 del sect
 
-kremlin=open("input.txt","rU").read()
+myinp = open("input.txt","rU").read()
 
 out = ""
 
 import re
 
-kremlin = re.compile(r"\{\{\s*EGS-link\s*\|",re.I).split(kremlin)
+myinp = re.compile(r"\{\{\s*EGS-link\s*\|",re.I).split(myinp)
 
-out += kremlin.pop(0)
+out += myinp.pop(0)
 
-for putin in kremlin:
-    wool,squeals = putin.split("}}",1)
-    wool = wool.split("|")
+for part in myinp:
+    tcall, text = part.split("}}",1)
+    tcall = tcall.split("|")
     sect = "story"
     datemode = 1
-    if wool[0].strip() == "sketchlost":
+    if tcall[0].strip() == "sketchlost":
         sect = "sketch"
-        wool.pop(0)
-        wool.append("usemirror="+wool.pop(0).strip())
-        squeals = " (lost, externally mirrored)"+squeals
-    elif wool[0].strip() == "bg":
-        wool.pop(0)
-        trailing="|".join([""]+wool) #Leading | iff not empty
-        out += "{{EGS-BG-link"+trailing+"}}"+squeals
+        tcall.pop(0)
+        tcall.append("usemirror="+tcall.pop(0).strip())
+        text = " (lost, externally mirrored)"+text
+    elif tcall[0].strip() == "bg":
+        tcall.pop(0)
+        trailing="|".join([""]+tcall) #Leading | iff not empty
+        out += "{{EGS-BG-link"+trailing+"}}"+text
         continue
-    elif wool[0].strip()[0] in "ns":
-        sect = wool[0].strip().rstrip("2")
-        if wool[0].strip()[-1]=="2":
+    elif tcall[0].strip()[0] in "ns":
+        sect = tcall[0].strip().rstrip("2")
+        if tcall[0].strip()[-1] == "2":
             datemode = 0
-        wool.pop(0)
+        tcall.pop(0)
     #
     if not datemode:
-        id=wool.pop(0).strip()
-        year=wool.pop(0).strip()
-        month=wool.pop(0).strip()
-        day=wool.pop(0).strip()
+        sid = tcall.pop(0).strip()
+        year = tcall.pop(0).strip()
+        month = tcall.pop(0).strip()
+        day = tcall.pop(0).strip()
     else:
-        year=wool.pop(0).strip()
-        month=wool.pop(0).strip()
-        day=wool.pop(0).strip()
+        year = tcall.pop(0).strip()
+        month = tcall.pop(0).strip()
+        day = tcall.pop(0).strip()
         try:
-            id=d2i[{"story":"ST","sketch":"SB","np":"NP"}[sect]][year+"-"+month+"-"+day]
+            sid = d2i[{"story":"ST","sketch":"SB","np":"NP"}[sect]][year+"-"+month+"-"+day]
         except KeyError:
-            id=None
-        if id[0]=="-":
-            id=None
+            sid = None
+        if sid[0] == "-":
+            sid = None
     #Note if "polysketch" in a title (unlikely) then we in trouble.
-    trailing=("|".join([""]+wool)).replace("polysketch","polyday") #Leading | iff not empty
+    trailing = ("|".join([""] + tcall)).replace("polysketch", "polyday") #Leading | iff not empty
     if not id: #i.e. none found
         out += "{{EGS-link-date|"+sect+"|"+year+"|"+month+"|"+day+trailing+"}}"
     else:
         out += "{{EGS-link-dateid|"+sect+"|"+id+"|"+year+"|"+month+"|"+day+trailing+"}}"
-    out += squeals
+    out += text
 
 open("output.txt","w").write(out)
 f=5
